e19f563d1e5176b875c3c7b2f106f8087b040a20
Christina Kashevar <christi_k@mail.ru>
2021-03-03 21:49:38 +0300

    del: src

--- src/components/OkButton/OkButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './OkButton.scss';
-
-const OkButton = ({onclick}) => (
-    <button className="save" onClick={onclick} >OK</button>
-)
-
-export default OkButton;
